8c9ab297d1d8a51ee5b05fc2f72c0108762d7783
Nikolay Martynov <tty0sh@gmail.com>
2015-01-08 17:28:55 +0400

    add GET and POST methods for REST API

--- notes.py
+++ notes.py
@@ -12,27 +12,61 @@ db.notes.insert({
 """
 import tornado.ioloop
 import tornado.web
-import tornado.escape
-
-from tornado.options import define, options
 
 import pymongo
+import json
+import logging as logger
 
 import settings
 
+from bson.json_util import dumps
+from bson.objectid import ObjectId
+
+from tornado.options import define, options
+
 define('port', default=8889, help='run on the given port', type=int)
+
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('notes/index.html')
 
 
-class NotesHandler(tornado.web.RequestHandler):
-    def get(self):
+class NotesHandler(tornado.web.RequestHandler): 
+    def get(self, _id=None):
         coll = self.application.db.notes
-        notes = coll.find()
-        self.render('notes/notes.html', notes=notes)
-     
+        result = []
+        try:
+            if _id:                
+                note = coll.find_one({"_id": ObjectId(_id)})
+                result = dumps(note) 
+            else:                
+                notes = coll.find()                
+                result = dumps(notes)
+
+            self.set_header('Content-Type', 'application/json')
+            self.set_status(200)
+            self.write(result)
+        
+        except Exception as e:
+            self.set_header('Content-Type', 'application/json')
+            self.set_status(400)
+            self.write({'error_message': '{0}'.format(e)})
+        #self.render('notes/notes.html', notes=notes)
+
+    
+    def post(self):
+        coll = self.application.db.notes
+        coll.insert(json.loads(self.request.body))
+        
+        self.set_header('Content-Type', 'application/json')
+        self.set_status(200)
+        self.write(self.request.body)
+
+    
+    def delete(self, _id=None):
+        pass    
+    
 
 
 class Application(tornado.web.Application):
@@ -50,7 +84,8 @@ class Application(tornado.web.Application):
 
 ROUTES = [
     (r"/", IndexHandler),
-    (r"/notes", NotesHandler)
+    (r"/notes", NotesHandler),
+    (r"/notes/(.*)", NotesHandler)
 ]
 
 
@@ -58,4 +93,5 @@ if __name__ == "__main__":
     tornado.options.parse_command_line()
     http_server = Application()
     http_server.listen(options.port)
+    logger.info('Tornado server is starting..')
     tornado.ioloop.IOLoop.instance().start()
