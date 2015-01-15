3c5f097c90993e41c66a616f1a359134d66ac01b
Nikolay Martynov <tty0sh@gmail.com>
2015-01-15 10:40:58 +0300

    add .bowerrc

--- api/test.py
+++ api/test.py
@@ -5,7 +5,8 @@ result = { key:(b[key] if key in b.keys() else None) for key in a }
 
 
 import bcrypt
-print bcrypt.hashpw('123', '$2a$12$37qs4pLr.RU06QfWuRjcL.f2xEurgVotVOUtwzQkWfpvegRtXT0DC') == '$2a$12$37qs4pLr.RU06QfWuRjcL.f2xEurgVotVOUtwzQkWfpvegRtXT0DC'
+print bcrypt.hashpw('123', '$2a$12$37qs4pLr.RU06QfWuRjcL.f2xEurgVotVOUtwzQkWfpvegRtXT0DC') \
+	== '$2a$12$37qs4pLr.RU06QfWuRjcL.f2xEurgVotVOUtwzQkWfpvegRtXT0DC'
 
 f = {1:1, 2:2}
 print any(f), all(f)
