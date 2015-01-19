6eb4a2d8e504be2cf4af0674974f60624677f8bd
Nikolay Martynov <tty0sh@gmail.com>
2015-01-19 11:33:39 +0400

    refactoring

--- api/test.py
+++ api/test.py
@@ -5,7 +5,7 @@ result = { key:(b[key] if key in b.keys() else None) for key in a }
 
 
 import bcrypt
-print bcrypt.hashpw('123', '$2a$12$37qs4pLr.RU06QfWuRjcL.f2xEurgVotVOUtwzQkWfpvegRtXT0DC') == '$2a$12$37qs4pLr.RU06QfWuRjcL.f2xEurgVotVOUtwzQkWfpvegRtXT0DC'
 
-f = {1:1, 2:2}
-print any(f), all(f)
+
+print bcrypt.hashpw('123')
+
